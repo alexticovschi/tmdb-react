f08a4d29baa3c97bc463a1fb40a6983412157c40
AlexTicovschi <alexmticovschi@gmail.com>
2018-10-27 11:59:11 +0100

    getting a movie list from themoviedb API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
 class App extends Component {
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  performSearch = async () => {
+    const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
+    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?query=marvel&api_key=${APIKEY}`);
+
+    const movies = await resp.json();
+    
+    this.setState({ movies: movies.results });
+    console.log({movies})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
