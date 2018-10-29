0569a63090a9ef0663bf498ad4820703b4f8961e
AlexTicovschi <alexmticovschi@gmail.com>
2018-10-29 20:51:28 +0000

    saving to local storage

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -12,6 +12,56 @@ class App extends Component {
     movies: []
   }
 
+  componentDidMount(){
+    this.hydrateStateWithLocalStorage();
+  }
+
+  componentWillUnmount() {
+    //window.onload = this.getRecipe();
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+    
+    // saves if component has a chance to unmount
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    // for every item in React state
+    for (let key in this.state) {
+      // save to localStorage
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
+  onSearchChange = (event) => {
+    event.preventDefault();
+
+    this.setState({ searchField: event.target.value });
+    console.log('[searchField]:',this.state.searchField);
+  }
+
   // componentDidMount() {
   //   this.performSearch();
   // }
