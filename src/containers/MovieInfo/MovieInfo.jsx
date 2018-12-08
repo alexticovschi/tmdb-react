fba604f005f584c0ff2ef28f4a5a7eb749af3d55
AlexTicovschi <alexmticovschi@gmail.com>
2018-12-08 13:44:22 +0000

    restyling the MovieInfo container

--- src/containers/MovieInfo/MovieInfo.jsx
+++ src/containers/MovieInfo/MovieInfo.jsx
@@ -4,84 +4,85 @@ import './MovieInfo.css';
 import SimilarMovieList from '../../components/SimilarMovieList/SimilarMovieList';
 
 class MovieInfo extends Component {
-  state = {
-    movie: [],
-    similar_movies: []
-  }
+    state = {
+        movie: [],
+        similar_movies: []
+    }
 
-  componentDidMount() {
-    const { movie_id } = this.props.match.params;
-    this.getMovieById(movie_id);
-    this.getSimilarMovies(movie_id);
-  }
+    componentDidMount() {
+        const { movie_id } = this.props.match.params;
+        this.getMovieById(movie_id);
+        this.getSimilarMovies(movie_id);
+    }
 
-  getMovieById = async (ID) => {
-    const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
-    // https://api.themoviedb.org/3/movie/tt4154756?api_key=9baa3cbfd9b62ea4f97966abadf41653
-    const resp = await fetch(`https://api.themoviedb.org/3/movie/${ID}?&api_key=${APIKEY}&language=en-US`);
+    getMovieById = async (ID) => {
+        const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
+        // https://api.themoviedb.org/3/movie/tt4154756?api_key=9baa3cbfd9b62ea4f97966abadf41653
+        const resp = await fetch(`https://api.themoviedb.org/3/movie/${ID}?&api_key=${APIKEY}&language=en-US`);
 
-        const movie = await resp.json();
+            const movie = await resp.json();
 
-        this.setState({ movie });
-        console.log('[MOVIE]:',this.state.movie);
-        console.log({movie});
-  }
+            this.setState({ movie });
+            console.log('[MOVIE]:',this.state.movie);
+            console.log({movie});
+    }
 
-  getSimilarMovies = async (ID) => {
-    const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
-    const resp = await fetch(`https://api.themoviedb.org/3/movie/${ID}/similar?&api_key=${APIKEY}&language=en-US&page=1`);
+    getSimilarMovies = async (ID) => {
+        const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
+        const resp = await fetch(`https://api.themoviedb.org/3/movie/${ID}/similar?&api_key=${APIKEY}&language=en-US&page=1`);
 
-    const movies = await resp.json();
+        const movies = await resp.json();
 
-    this.setState({ similar_movies: movies.results });
-    console.log('[MOVIE]:',this.state.similar_movies);
-    console.log({movies});
-  }
+        this.setState({ similar_movies: movies.results });
+        console.log('[MOVIE]:',this.state.similar_movies);
+        console.log({movies});
+    }
 
-  render() {
-    const {movie} = this.state;
-    const base_url = 'https://image.tmdb.org/t/p/w342';
-    const genres = movie.genres;
-    let list = genres && genres.map(g => g.name + ' ');
+    render() {
+        const {movie} = this.state;
+        const base_url2 = 'https://image.tmdb.org/t/p/w342';
+        const base_url1 = 'https://image.tmdb.org/t/p/original';
+        const genres = movie.genres;
+        let list = genres && genres.map(g => g.name + ' ');
+        return (
+        <div className="box">
+            <div className="flex-container">
+                <div className="box-left">
+                    <img className="img-info_original" src={base_url1 + movie.poster_path} alt={"img card"} />
+                </div>
 
-    return (
-      <div className="box">
-        <div className="flex-container">
-          <div className="d1">
-            <img className="img-info" src={base_url + movie.poster_path} alt={"img card"} />
-          </div>
-          <div className="d2">
-              <h1 className="info-title"><strong>{movie.original_title}</strong></h1> 
-              <hr/>
-              {list !== null ? <p><strong>Genre:</strong>  {list}</p> : null}
-              <p><strong>Released: </strong>  {movie.release_date}</p>
-              <p><strong>Tagline: </strong>  {movie.tagline}</p> 
-              {movie.BoxOffice ? <p><strong>BoxOffice: </strong>  {movie.BoxOffice}</p> : null}
-              {movie.homepage ? <p><strong>Website: </strong>  <a href={movie.homepage} target="_blank" rel="noopener noreferrer">{movie.homepage}</a></p> : null}
-          </div> 
-          <div className="d3">
-            <p className="plot"><strong>Plot: </strong>  {movie.overview}</p>
-            <div className="btn-div">
-              <a className="btn btn-info b1" href={`http://imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noopener noreferrer">View on IMDB</a>
-              <button className="btn btn-info b2" onClick={() => this.props.history.push('/')}>Back To Search</button>
+                <div className="box-right">
+                    <h1 className="info-title"><strong>{movie.original_title}</strong></h1> 
+                    <hr/>
+                    {list !== null ? <p><strong>Genre:</strong>  {list}</p> : null}
+                    <p><strong>Released: </strong>  {movie.release_date}</p>
+                    <p><strong>Tagline: </strong>  {movie.tagline}</p> 
+                    {movie.BoxOffice ? <p><strong>BoxOffice: </strong>  {movie.BoxOffice}</p> : null}
+                    {movie.homepage ? <p><strong>Website: </strong>  <a href={movie.homepage} target="_blank" rel="noopener noreferrer">{movie.homepage}</a></p> : null}
+                
+                    <p className="plot"><strong>Plot: </strong>  {movie.overview}</p>
+                    <div className="btn-div">
+                        <a className="btn btn-info b1" href={`http://imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noopener noreferrer">View on IMDB</a>
+                        <button className="btn btn-info b2" onClick={() => this.props.history.push('/')}>Back To Search</button>
+                    </div>
+                </div> 
             </div>
-          </div>
 
-          {this.state.similar_movies.length > 0 ?
-                <div className="d4">
-                {/* <div className="d4" style={{height: 500, overflowX: "hidden", overflowY: "auto"}}> */}
+            <div className="flex-container">
+            {this.state.similar_movies.length > 0 ?
+                <div className="similar_movies">
                     <h1>Similar Movies</h1>
                     <SimilarMovieList
                         movieList={this.state.similar_movies}
                         getMovieById={this.getMovieById} />
+                            
                     <button className="btn btn-info b3" onClick={() => this.props.history.push('/')}>Back To Search</button>
-
                 </div>
-            : null}
+                : null}
+            </div>
         </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default MovieInfo;
