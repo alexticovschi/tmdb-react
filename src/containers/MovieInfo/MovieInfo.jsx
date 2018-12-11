88e832d503d55a160548967e954302359de77a5c
AlexTicovschi <alexmticovschi@gmail.com>
2018-12-11 17:02:11 +0000

    getting and displaying trailers

--- src/containers/MovieInfo/MovieInfo.jsx
+++ src/containers/MovieInfo/MovieInfo.jsx
@@ -3,13 +3,16 @@ import SimilarMovieList from '../../components/SimilarMovieList/SimilarMovieList
 import Loader from '../../components/Loader/Loader';
 import ActorImageCard from '../../components/ActorImageCard/ActorImageCard';
 
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import './MovieInfo.css';
 
 class MovieInfo extends Component {
     state = {
         movie: [],
         credits: [],
-        similar_movies: []
+        similar_movies: [],
+        trailers: []
     }
 
     componentDidMount() {
@@ -17,6 +20,8 @@ class MovieInfo extends Component {
         this.getMovieById(movie_id);
         this.getSimilarMovies(movie_id);
         this.getMovieCredits(movie_id);
+        // this.getRecomendations(movie_id);
+        this.getTrailers(movie_id);
     }
 
     getMovieById = async (ID) => {
@@ -27,8 +32,8 @@ class MovieInfo extends Component {
             const movie = await resp.json();
 
             this.setState({ movie });
-            console.log('[MOVIE]:',this.state.movie);
-            console.log({movie});
+            // console.log('[MOVIE]:',this.state.movie);
+            // console.log({movie});
     }
 
     getSimilarMovies = async (ID) => {
@@ -38,8 +43,8 @@ class MovieInfo extends Component {
         const movies = await resp.json();
 
         this.setState({ similar_movies: movies.results });
-        console.log('[MOVIE]:',this.state.similar_movies);
-        console.log({movies});
+        // console.log('[MOVIE]:',this.state.similar_movies);
+        // console.log({movies});
     }
 
     getMovieCredits = async (ID) => {
@@ -48,54 +53,89 @@ class MovieInfo extends Component {
     
         const credits = await resp.json();
         this.setState({ credits: credits.cast });
-        console.log('[CREDITS]', credits);
+        // console.log('[CREDITS]', credits);
+    }
+
+    // getRecomendations = async (ID) => {
+    //     const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
+    //     const resp = await fetch(`https://api.themoviedb.org/3/movie/${ID}/recommendations?&api_key=${APIKEY}&language=en-US`);
+
+    //     const recomendations = await resp.json();
+    //     this.setState({ recomendations: recomendations });
+    //     console.log('[recomendations]', recomendations);
+    // }
+
+    getTrailers = async (ID) => {
+        const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
+        const resp = await fetch(`https://api.themoviedb.org/3/movie/${ID}/videos?&api_key=${APIKEY}&language=en-US`);
+
+        const trailers = await resp.json();
+        this.setState({ trailers: trailers.results });
+        console.log('[trailers]', trailers);
     }
 
     render() {
         const {movie} = this.state;
-        // const base_url2 = 'https://image.tmdb.org/t/p/w342';
         const base_url = 'https://image.tmdb.org/t/p/w780';
         const genres = movie.genres;
         let list = genres && genres.map(g => g.name + ' ');
 
-        console.log(this.state.credits);
+        // console.log(this.state.credits);
         return (
             <div className="box">
-                <div className="flex-container">
-                    <div className="box-left">
-                        <img className="img-info" src={base_url + movie.poster_path} alt={"img card"} />
-                    </div>
-
-                    <div className="box-right">
-                        <h1 className="info-title"><strong>{movie.original_title}</strong></h1> 
-                        <hr/>
-                        {list !== null ? <p><strong>Genre:</strong>  {list}</p> : null}
-                        <p><strong>Released: </strong>  {movie.release_date}</p>
-                        <p><strong>Tagline: </strong>  {movie.tagline}</p> 
-                        {movie.BoxOffice ? <p><strong>BoxOffice: </strong>  {movie.BoxOffice}</p> : null}
-                        {movie.homepage ? <p><strong>Website: </strong>  <a href={movie.homepage} target="_blank" rel="noopener noreferrer">{movie.homepage}</a></p> : null}
-                    
-                        <p className="plot"><strong>Plot: </strong>  {movie.overview}</p>
-                        <div className="btn-div">
-                            <a className="btn btn-info b1" href={`http://imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noopener noreferrer">View on IMDB</a>
-                            <button className="btn btn-info b2" onClick={() => this.props.history.push('/')}>Back To Search</button>
+                <div className="container">
+                    <div className="row">
+                        <div className="box-left">
+                            <img className="img-info" src={base_url + movie.poster_path} alt={"img card"} />
                         </div>
-                    </div> 
+                        <div className="box-right">
+                            <div className="inner-box-right">
+                                <h1 className="info-title">{movie.original_title}</h1> 
+                                <hr/>
+                                {list !== null ? <p><strong>Genre:</strong>  {list}</p> : null}
+                                <p><strong>Released: </strong>  {movie.release_date}</p>
+                                <p><strong>Tagline: </strong>  {movie.tagline}</p> 
+                                {movie.BoxOffice ? <p><strong>BoxOffice: </strong>  {movie.BoxOffice}</p> : null}
+                                {movie.homepage ? <p><strong>Website: </strong>  <a href={movie.homepage} target="_blank" rel="noopener noreferrer">{movie.homepage}</a></p> : null}
+                                
+                                <div className="btn-div">
+                                    <a className="btn btn-info b1" href={`http://imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noopener noreferrer">View on IMDB</a>
+                                    <button className="btn btn-info b2" onClick={() => this.props.history.push('/')}>Back To Search</button>
+                                </div>
+                            </div>
+                        
+                        </div>
+                    </div>    
                 </div>
 
-                <div className="container cast">
-                    <h1 style={{color: '#fff', textAlign: 'center'}}><strong>Full Cast</strong></h1>
+                <div className="flex-container">
+                    <div className="main-content" style={{marginTop:"40px"}}>
+                        {this.state.trailers.map(trailer => (
+                            <div key={trailer.key}>
 
-                    <main className="main-content">
-                        {this.state.credits &&
-                            this.state.credits.map(actor => {
-                            
-                            return (
-                                <ActorImageCard actor={actor}/>
-                            )
-                            }
-                        )}
-                    </main>
+                                <iframe 
+                                    className="iframe-card"
+                                    style={{borderRadius:"6px", margin:"10px auto"}} 
+                                    title="1" 
+                                    width="400" 
+                                    height="200"
+                                    src={`https://www.youtube.com/embed/${trailer.key}`}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="container">
+                        <h1 style={{color: '#fff', textAlign: 'center'}}><strong>Full Cast</strong></h1>
+
+                        <main className="main-content">
+                            {this.state.credits &&
+                                this.state.credits.map(actor => {
+                                    return <ActorImageCard key={actor.id} actor={actor}/>
+                                }
+                            )}
+                        </main>
                 </div>
 
                 <div className="flex-container">
