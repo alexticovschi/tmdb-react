7ffbc3e1873d444ebf657fb639814536f8d825bf
AlexTicovschi <alexmticovschi@gmail.com>
2018-12-14 19:07:36 +0000

    adding a Carousel component

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import './Carousel.css';
+
+import { Link } from 'react-router-dom';
+
+
+export default ({movies}) => {
+    // console.log(movies);
+    const base_url = 'https://image.tmdb.org/t/p/original';
+    return (
+        <Carousel autoPlay>
+            {movies.map(movie => (
+                <div key={movie.id}>
+                    <img className="carousel-img" src={base_url + movie.backdrop_path} alt="carousel img"/>
+                    <Link 
+                        to={`/movie-info/${movie.id}`} 
+                        className="legend">
+                            <span className="movie-title">{movie.title} </span>- Release date: {movie.release_date}
+                    </Link>
+                </div>
+            ))}
+        </Carousel>
+    );
+};
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import Carousel from "../../components/Carousel/Carousel";
 import Navbar from "../../components/Navbar/Navbar";
+import Loader from "../../components/Loader/Loader";
 
+
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './Home.css';
 
 class Home extends Component {
@@ -18,48 +23,31 @@ class Home extends Component {
         this.getTopRatedMovies();
     }
 
-    getLatestMovies = async (ID) => {
-        const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
-        const resp = await fetch(`https://api.themoviedb.org/3/movie/latest?&api_key=${APIKEY}&language=en-US`);
-
-        const latest = await resp.json();
-
-        this.setState({ latest });
-    }
-
     getUpcomingMovies = async (ID) => {
         const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
         const resp = await fetch(`https://api.themoviedb.org/3/movie/upcoming?&api_key=${APIKEY}&language=en-US`);
-
         const upcomingMovies = await resp.json();
-
         this.setState({ upcomingMovies: upcomingMovies.results });
     }
 
     getNowPlayingMovies = async (ID) => {
         const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
         const resp = await fetch(`https://api.themoviedb.org/3/movie/now_playing?&api_key=${APIKEY}&language=en-US`);
-
         const nowPlayingMovies = await resp.json();
-
         this.setState({ nowPlayingMovies: nowPlayingMovies.results });
     }
 
     getPopularMovies = async (ID) => {
         const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
         const resp = await fetch(`https://api.themoviedb.org/3/movie/popular?&api_key=${APIKEY}&language=en-US`);
-
         const popularMovies = await resp.json();
-
         this.setState({ popularMovies: popularMovies.results });
     }
 
     getTopRatedMovies = async (ID) => {
         const APIKEY = '9baa3cbfd9b62ea4f97966abadf41653';
         const resp = await fetch(`https://api.themoviedb.org/3/movie/top_rated?&api_key=${APIKEY}&language=en-US`);
-
         const topRatedMovies = await resp.json();
-
         this.setState({ topRatedMovies: topRatedMovies.results });
     }
 
@@ -67,12 +55,18 @@ class Home extends Component {
         const upcoming = this.state.upcomingMovies;
         const popular = this.state.popularMovies;
         const toprated = this.state.topRatedMovies;
-
-        console.log('upcoming:',upcoming, 'popular:',popular, 'toprated:', toprated)
-
         return (
             <div>
-                Home
+                <Navbar/>
+                <main className="container" style={{ marginTop: "56px" }}>
+                    <Carousel movies={this.state.nowPlayingMovies} />    
+
+
+                </main>
+
+                <footer></footer>
+
+                <Loader/>
             </div>
 
         )
