feaa416f6a9e348580a72ea6e005f1dba77a7df3
AlexTicovschi <alexmticovschi@gmail.com>
2018-12-25 11:00:39 +0000

    getting movies on key press

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import './SearchBox.css';
 
 const SearchBox = ({ onSearchChange, getMovies }) => {
-  const onFormSubmit = (e) => {
-    e.preventDefault();
-  }
-  return (
-    <form onSubmit={onFormSubmit}>
-      <input onChange={onSearchChange} className="searchBox" type="search" name="search" placeholder="Search Movie..." />
-      <input onClick={getMovies} type="button" value="Search" />
-    </form>
-  );
-};
+    const onFormSubmit = (e) => e.preventDefault();
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <input 
+                onChange={onSearchChange} 
+                onKeyPress={getMovies} 
+                className="searchBox" type="search" 
+                name="search" 
+                placeholder="Search Movie..." 
+            />
+            <input 
+                onClick={getMovies} 
+                className="searchBoxBtn" 
+                type="button" 
+                value="Search" 
+            />
+        </form>
+    );
+  };
 
 export default SearchBox;
