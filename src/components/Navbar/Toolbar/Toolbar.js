af932aca31413ad7be6403a9da81400146a0f8ba
AlexTicovschi <alexmticovschi@gmail.com>
2019-01-13 10:07:46 +0000

    moving toolbar toggle button to the right

--- src/components/Navbar/Toolbar/Toolbar.js
+++ src/components/Navbar/Toolbar/Toolbar.js
@@ -6,11 +6,10 @@ import "./Toolbar.css";
 const Toolbar = props => (
   <header className="toolbar">
     <nav className="toolbar__navigation">
-      <div className="toolbar__toggle-button">
-        <DrawerToggleButton click={props.drawerClickHandler} />
-      </div>
       <div className="toolbar__logo">
-        <a href="/">TMDB</a>
+        <a href="/">
+          <img src="https://www.themoviedb.org/assets/1/v4/logos/powered-by-rectangle-green-dcada16968ed648d5eb3b36bbcfdd8cdf804f723dcca775c8f2bf4cea025aad6.svg" alt=""/>
+        </a>
       </div>
       <div className="spacer" />
       <div className="toolbar_navigation-items">
@@ -19,7 +18,7 @@ const Toolbar = props => (
             <a href="/">Home</a>
           </li>
           <li>
-            <a href="/movies">Movies</a>
+            <a href="/movies/now-playing">Movies</a>
           </li>
           <li>
             <a href="/tv-shows">TV Shows</a>
@@ -31,6 +30,9 @@ const Toolbar = props => (
             <a href="/login">Login </a>
           </li>
         </ul>
+      </div>
+      <div className="toolbar__toggle-button">
+        <DrawerToggleButton click={props.drawerClickHandler} />
       </div>
     </nav>
   </header>
