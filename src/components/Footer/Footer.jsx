7e0fb4399e5c8d26b8220567d5cfd18f628cc697
AlexTicovschi <alexmticovschi@gmail.com>
2019-01-18 11:06:47 +0000

    adding footer info & styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,17 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer>
-
+                <div className="f-logo">
+                    <a href="https://developers.themoviedb.org/3/getting-started/introduction" target="_blank">
+                        <img className="footer-logo" src="https://www.themoviedb.org/assets/1/v4/logos/powered-by-square-green-11c0c7f8e03c4f44aa54d5e91d9531aa9860a9161c49f5fa741b730c5b21a1f2.svg" alt="tmdb logo"/>
+                    </a>
+                </div>
+                <div className="social-media">
+                    <a href="https://www.freecodecamp.org/alexticovschi" class="fa fa-free-code-camp"></a>
+                    <a href="https://codepen.io/AlexTicovschi" class="fa fa-codepen"></a>
+                    <a href="https://github.com/alexticovschi/tmdb-react" class="fa fa-github"></a>
+                    <a href="https://www.linkedin.com/in/alex-ticovschi-4b3832134/" class="fa fa-linkedin"></a>
+                </div>
         </footer>
     );
 };
