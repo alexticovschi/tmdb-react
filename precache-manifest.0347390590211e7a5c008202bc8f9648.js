162792e33a483400c98886223055d58bff920b59
AlexTicovschi <alexmticovschi@gmail.com>
2019-01-27 18:23:41 +0000

    Updates

---
+++ precache-manifest.0347390590211e7a5c008202bc8f9648.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9ef57d841424e74f23545dd34100d63",
+    "url": "/movies/static/media/search.a9ef57d8.svg"
+  },
+  {
+    "revision": "6ad4d8afb5a54c964d9f",
+    "url": "/movies/static/js/runtime~main.6ad4d8af.js"
+  },
+  {
+    "revision": "a8549912412e5c491bc4",
+    "url": "/movies/static/js/main.a8549912.chunk.js"
+  },
+  {
+    "revision": "4c1b1cd71e265f8485ff",
+    "url": "/movies/static/js/1.4c1b1cd7.chunk.js"
+  },
+  {
+    "revision": "a8549912412e5c491bc4",
+    "url": "/movies/static/css/main.49ded945.chunk.css"
+  },
+  {
+    "revision": "4c1b1cd71e265f8485ff",
+    "url": "/movies/static/css/1.8b47717b.chunk.css"
+  },
+  {
+    "revision": "a44611c70877e21c0d0dcceb09e01035",
+    "url": "/movies/index.html"
+  }
+];
