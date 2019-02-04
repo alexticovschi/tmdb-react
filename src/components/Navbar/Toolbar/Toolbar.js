7c4f27900ccd3a0a6d6184e67e9066ade488de66
AlexTicovschi <alexmticovschi@gmail.com>
2019-02-04 22:41:32 +0000

    navbar style tweak

--- src/components/Navbar/Toolbar/Toolbar.js
+++ src/components/Navbar/Toolbar/Toolbar.js
@@ -5,36 +5,38 @@ import "./Toolbar.css";
 
 const Toolbar = props => (
   <header className="toolbar">
-    <nav className="toolbar__navigation">
-      <div className="toolbar__logo">
-        <a href="/">
-          <img src="https://www.themoviedb.org/assets/1/v4/logos/powered-by-rectangle-green-dcada16968ed648d5eb3b36bbcfdd8cdf804f723dcca775c8f2bf4cea025aad6.svg" alt=""/>
-        </a>
-      </div>
-      <div className="spacer" />
-      <div className="toolbar_navigation-items">
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/movies/now-playing">Movies</a>
-          </li>
-          <li>
-            <a href="/tv-shows">TV Shows</a>
-          </li>
-          <li>
-            <a href="/genres/80">Genres</a>
-          </li>
-          <li>
-            <a href="/login">Login </a>
-          </li>
-        </ul>
-      </div>
-      <div className="toolbar__toggle-button">
-        <DrawerToggleButton isOpen={props.isOpen} click={props.drawerClickHandler} />
-      </div>
-    </nav>
+    <div className="toolbar-wrapper">
+      <nav className="toolbar__navigation">
+        <div className="toolbar__logo">
+          <a href="/">
+            <img src="https://www.themoviedb.org/assets/1/v4/logos/powered-by-rectangle-green-dcada16968ed648d5eb3b36bbcfdd8cdf804f723dcca775c8f2bf4cea025aad6.svg" alt=""/>
+          </a>
+        </div>
+        <div className="spacer" />
+        <div className="toolbar_navigation-items">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/movies/now-playing">Movies</a>
+            </li>
+            <li>
+              <a href="/tv-shows">TV Shows</a>
+            </li>
+            <li>
+              <a href="/genres/80">Genres</a>
+            </li>
+            <li>
+              <a href="/login">Login </a>
+            </li>
+          </ul>
+        </div>
+        <div className="toolbar__toggle-button">
+          <DrawerToggleButton isOpen={props.isOpen} click={props.drawerClickHandler} />
+        </div>
+      </nav>
+    </div>
   </header>
 );
 
